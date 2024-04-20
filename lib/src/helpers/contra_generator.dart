ab81d27662310e4aedfadd674df3f8f69384429c
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2024-04-20 10:46:52 +0000

    chore: added uniquekey as a generator

--- lib/src/helpers/contra_generator.dart
+++ lib/src/helpers/contra_generator.dart
@@ -1,10 +1,12 @@
-import 'dart:math';
+import 'package:flutter/foundation.dart';
 
 class ContraGenerator {
   static String generateId() {
     //TODO use UUID;
-    final millisecondsSinceEpoch = DateTime.now().millisecondsSinceEpoch;
-    final random = Random().nextInt(999999);
-    return '$millisecondsSinceEpoch-$random';
+    // final millisecondsSinceEpoch = DateTime.now().millisecondsSinceEpoch;
+    // final random = Random().nextInt(999999);
+
+    final code = UniqueKey().hashCode;
+    return code.toString();
   }
 }
